7755c0c9f2009ae7c91be9da6c018b2bdde2fcda
cagataySert <cagatay.sert5@gmail.com>
2019-06-11 02:25:43 +0300

    photobox removed and changePhotoLikeStatus actioncreator was added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Image, ActivityIndicator, FlatList, Dimensions } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import { View, ActivityIndicator, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { getPhotos } from '../../actions/PhotoActions';
 import Seperator from '../../components/Seperator';
+import PhotoBox from './PhotoBox';
+import { changePhotoLikeStatus } from '../../actions/PhotoActions';
 
-
-const { width, height } = Dimensions.get('window');
 
 class Home extends Component {
 
@@ -19,6 +18,10 @@ class Home extends Component {
     this.props.getPhotos();
   }
 
+  handleLikePhoto = (photoId, isLiked) => {
+    console.log(photoId, isLiked);
+    this.props.changePhotoLikeStatus(photoId, isLiked);
+  }
 
   render() {
     const userData = this.props.user;
@@ -34,49 +37,7 @@ class Home extends Component {
           keyExtractor={(item, index) => index.toString()}
           ItemSeparatorComponent={Seperator}
           renderItem={({ item }) =>
-            <View style={{ height: height * 0.9, backgroundColor: '' }}>
-
-              <View style={{ flex: 1.25, flexDirection: 'row', backgroundColor: '', borderBottomColor: 'black', borderBottomWidth: 0.5 }}>
-
-                <View style={{ flex: 8, flexDirection: 'row', alignItems: 'center' }}>
-                  <Image
-                    style={{ width: 50, height: 50, borderRadius: 25, marginLeft: 7 }}
-                    source={{ uri: 'https://galeri13.uludagsozluk.com/633/sozluk-yazarlarinin-profil-resmi_1363249.png' }}
-                  />
-
-                  <Text style={{ padding: 10 }}>
-                    {userData.firstName} {userData.lastName}
-                  </Text>
-                </View>
-
-
-                <View style={{ flex: 2, backgroundColor: '', justifyContent: 'center', alignItems: 'flex-end', padding: 10 }}>
-                  <Text>2d</Text>
-                </View>
-              </View>
-
-
-              <View style={{ flex: 7.5, borderBottomColor: 'black', borderBottomWidth: 0.5 }}>
-                <Image
-                  style={{ flex: 1 }}
-                  source={{ uri: item }}
-                />
-              </View>
-
-              <View style={{ flex: 1.25, flexDirection: 'row', backgroundColor: '' }}>
-
-                <View style={{ flex: 8, flexDirection: 'row', alignItems: 'center' }}>
-                  <Icon style={{ marginLeft: 10 }} size={20} name='heart' color='black' />
-                  <Icon style={{ marginLeft: 10 }} size={20} name='comment' color='black' />
-                  <Icon style={{ marginLeft: 10 }} size={20} name='share' color='black' />
-                </View>
-
-                <View style={{ flex: 2, alignItems: 'flex-end', marginRight: 7, justifyContent: 'center' }}>
-                  <Icon style={{ marginLeft: 7, marginRight: 5 }} size={20} name='ellipsis-h' color='black' />
-                </View>
-
-              </View>
-            </View>
+            <PhotoBox photoUrl={item.url} photoId={item.id} userData={userData} handleLikePhoto={this.handleLikePhoto} />
           }
         />
     );
@@ -96,4 +57,4 @@ const mapStateToProps = ({ authResponse, photoResponse }) => {
 }
 
 
-export default connect(mapStateToProps, { getPhotos })(Home);
+export default connect(mapStateToProps, { getPhotos, changePhotoLikeStatus })(Home);
